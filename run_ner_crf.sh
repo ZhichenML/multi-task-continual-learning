886715bafa8322b65b002d9df5dbaac0a2ec4661
Zhichen <v123gz@163.com>
2021-10-07 19:52:51 +0800

    Add files via upload

--- run_ner_crf.sh
+++ run_ner_crf.sh
@@ -3,7 +3,7 @@ CURRENT_DIR=`pwd`
 export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/bert-base-chinese
 export DATA_DIR=$CURRENT_DIR/datasets
 export OUTPUR_DIR=$CURRENT_DIR/outputs
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=4
 TASK_NAME="cner"
 #
 python run_ner_crf_cl_zcg.py \
@@ -16,8 +16,8 @@ python run_ner_crf_cl_zcg.py \
   --data_dir=$DATA_DIR/${TASK_NAME}/ \
   --train_max_seq_length=35 \
   --eval_max_seq_length=35 \
-  --per_gpu_train_batch_size=256 \
-  --per_gpu_eval_batch_size=256 \
+  --per_gpu_train_batch_size=512 \
+  --per_gpu_eval_batch_size=512 \
   --learning_rate=3e-5 \
   --crf_learning_rate=1e-3 \
   --num_train_epochs=20 \
