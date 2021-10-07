2c5d3765ad57798e6653763626eab95828c9abd0
Zhichen <v123gz@163.com>
2021-10-07 16:33:31 +0800

    Add files via upload

--- CL_algo/ewc.py
+++ CL_algo/ewc.py
@@ -30,17 +30,18 @@ class EWC_LOSS:
                 
     def _update_fisher_params(self, dl, num_batch, num_labels):
             # dl = DataLoader(current_ds, batch_size, shuffle=True)
-            log_liklihoods_ner = []
-            log_liklihoods_cates = []
+            
             
             for i, batch in enumerate(dl):
-                print("batch size", len(batch))
+                log_liklihoods_ner = []
+                log_liklihoods_cates = []
+                
                 if i > num_batch:
                     break
                 batch = tuple(t.to(torch.device("cuda")) for t in batch)
   
                 inputs = {"input_ids": batch[0], "attention_mask": batch[1], "token_type_ids":batch[2], "labels": batch[3],'cates_ids': batch[5],'acts_ids': batch[6]}
-                print(i)
+                
                 outputs, loss_cates, logits_cates = self.model(**inputs)
                 logits_ner = outputs[1]
                 
@@ -61,23 +62,25 @@ class EWC_LOSS:
                 log_likelihood_ner = torch.cat(log_liklihoods_ner).mean()
             
                 log_likelihood_cates = torch.cat(log_liklihoods_cates).mean()
-                grad_log_liklihood1 = autograd.grad(log_likelihood_ner, self.model.parameters(), allow_unused=True, retain_graph=True) #+ \
+                para = [v for v in self.model.parameters() if v.requires_grad==True]
+                grad_log_liklihood1 = autograd.grad(log_likelihood_ner, para, allow_unused=True, retain_graph=True) #+ \
                 grad_log_liklihood1 = [torch.tensor(0) if v == None else v for v in grad_log_liklihood1]
                 self.model.zero_grad()
-                grad_log_liklihood2 = autograd.grad(log_likelihood_cates, self.model.parameters(), allow_unused=True, retain_graph=True)
+                grad_log_liklihood2 = autograd.grad(log_likelihood_cates, para, allow_unused=True, retain_graph=True)
                 grad_log_liklihood2 = [torch.tensor(0) if v == None else v for v in grad_log_liklihood2]
                 
                 grad_log_liklihood = grad_log_liklihood1 + grad_log_liklihood2
 
         
                 _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
-                for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
-                    
-                    if hasattr(self.model, '{}_estimated_fisher'.format(_buff_param_name)):
-                        estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
-                        self.model.register_buffer(_buff_param_name+'_estimated_fisher', estimated_fisher+param.data.clone() ** 2)
-                    else:
+                if i == 0:
+                    for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
                         self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+                else:
+                    estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
+                    self.model.register_buffer(_buff_param_name+'_estimated_fisher', estimated_fisher+param.data.clone() ** 2)
+                    
+                        
 
                 gc.collect()
 
@@ -116,10 +119,11 @@ class EWC_LOSS_span:
                 
     def _update_fisher_params(self, dl, num_batch, num_labels):
             # dl = DataLoader(current_ds, batch_size, shuffle=True)
-            log_liklihoods_ner_start = []
-            log_liklihoods_ner_end = []
-            log_liklihoods_cates = []
+            
             for i, batch in enumerate(dl):
+                log_liklihoods_ner_start = []
+                log_liklihoods_ner_end = []
+                log_liklihoods_cates = []
                 if i > num_batch:
                     break
                 batch = tuple(t.to(torch.device("cuda")) for t in batch)
@@ -155,23 +159,40 @@ class EWC_LOSS_span:
                 output_cates = F.log_softmax(logits_cates, dim=1)
                 log_liklihoods_cates.append(torch.gather(output_cates, dim=1, index=inputs["cates_ids"].unsqueeze(-1)) )
 
-            # 3. mean likelihood and grad
-            log_likelihood_ner_start = torch.cat(log_liklihoods_ner_start).mean()
-            log_likelihood_ner_end = torch.cat(log_liklihoods_ner_end).mean()
-            log_likelihood_cates = torch.cat(log_liklihoods_cates).mean()
+                # 3. mean likelihood and grad
+                log_likelihood_ner_start = torch.cat(log_liklihoods_ner_start).mean()
+                log_likelihood_ner_end = torch.cat(log_liklihoods_ner_end).mean()
+                log_likelihood_cates = torch.cat(log_liklihoods_cates).mean()
 
-            grad_log_liklihood_ner_start = autograd.grad(log_likelihood_ner_start, self.model.parameters(), allow_unused=True, retain_graph=True)
-            grad_log_liklihood_ner_start = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_ner_start]
-            grad_log_liklihood_ner_end = autograd.grad(log_likelihood_ner_end, self.model.parameters(), allow_unused=True, retain_graph=True)
-            grad_log_liklihood_ner_end = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_ner_end]
-            grad_log_liklihood_cates = autograd.grad(log_likelihood_cates, self.model.parameters(), allow_unused=True)
-            grad_log_liklihood_cates = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_cates]
-            grad_log_liklihood = grad_log_liklihood_ner_start + grad_log_liklihood_ner_end + grad_log_liklihood_cates
+                grad_log_liklihood_ner_start = autograd.grad(log_likelihood_ner_start, self.model.parameters(), allow_unused=True, retain_graph=True)
+                grad_log_liklihood_ner_start = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_ner_start]
+                grad_log_liklihood_ner_end = autograd.grad(log_likelihood_ner_end, self.model.parameters(), allow_unused=True, retain_graph=True)
+                grad_log_liklihood_ner_end = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_ner_end]
+                grad_log_liklihood_cates = autograd.grad(log_likelihood_cates, self.model.parameters(), allow_unused=True)
+                grad_log_liklihood_cates = [torch.tensor(0) if v == None else v for v in grad_log_liklihood_cates]
+                grad_log_liklihood = grad_log_liklihood_ner_start + grad_log_liklihood_ner_end + grad_log_liklihood_cates
 
 
-            _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
-            for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
-                self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+                # _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
+                # for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
+                    
+                #     if hasattr(self.model, '{}_estimated_fisher'.format(_buff_param_name)):
+                #         estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
+                #         self.model.register_buffer(_buff_param_name+'_estimated_fisher', estimated_fisher+param.data.clone() ** 2)
+                #     else:
+                #         self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+                _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
+                if i == 0:
+                    for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
+                        self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+                else:
+                    estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
+                    self.model.register_buffer(_buff_param_name+'_estimated_fisher', estimated_fisher+param.data.clone() ** 2)
+                    
+
+                gc.collect()
+
+                torch.cuda.empty_cache()
 
     def _update_mean_params(self):
         for param_name, param in self.model.named_parameters():
